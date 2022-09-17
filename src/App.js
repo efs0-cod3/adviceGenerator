6f154e9f13c65c109d9ae427f2c7ccf5a0013630
efs0-cod3 <eflearnng@gmail.com>
2022-09-17 17:52:21 -0400

    advice gen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+
+import dice from "./images/icon-dice.svg"
+import "./App.scss";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [isLoading, setLoading] = useState(true);
+  const [ advice, setAdvice ] = useState('')
+
+  const getAdvice = async() => {
+    try {
+      const res = await fetch(`https://api.adviceslip.com/advice/${Math.round(Math.random()*224)}`)
+      const data = await res.json()
+      setAdvice(data.slip)
+      setLoading(false)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => getAdvice, [])
+
+if(isLoading){
+  return(
+    <main>
+    {/* <h3 className="loading">Loading...</h3>
+     */}
+     <div class="col-3">
+        <div class="snippet" data-title=".dot-windmill">
+          <div class="stage">
+            <div class="dot-windmill"></div>
+          </div>
+        </div>
+      </div>
+  </main>
+  )
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main>
+      <div className="container">
+        <h3>ADVICE #{advice.id}</h3>
+        <h1>{`“${advice.advice}”`}</h1>
+        <div className="divider"></div>
+        <div className="diceContainer">
+          <img src={dice} alt="dice" onClick={getAdvice}/>
+        </div>
+      </div>
+    </main>
+      <footer>
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by <a href="https://erickfelix.netlify.app/" target="_black" rel="noreferrer">Erick Felix</a>.
+      </footer>
     </div>
   );
 }
